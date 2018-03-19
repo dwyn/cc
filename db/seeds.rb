1210794bdf42ed6b16ec73370a6c27ccbb597e61
d w a y n e <dwayne.harmon@gmail.com>
2018-03-18 21:10:27 -0500

    Seeded (Partially).

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Language.create!(
-  name: 'Ruby'
+  name: 'Ruby',
+  section_id: 1
 )
 
 Section.create!(
   title: 'CLI Gem',
   description: 'Utilizing Object Oriented Programming to build and Publish a Ruby Gem.',
   language_id: 1
-  # raise.inspect
-  binding.pry
 )
 
 Concept.create!(
