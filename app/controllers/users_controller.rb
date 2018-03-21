1f187156f60869457db303e4f7579f386b5f985f
d w a y n e <dwayne.harmon@gmail.com>
2018-03-20 21:45:31 -0500

    Views.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def show
     @user = User.find(params[:id])
   end
@@ -7,5 +6,4 @@ class UsersController < ApplicationController
   def user_submitted
     @concepts = current_user.concepts.submitted_concepts
   end
-
 end
