599f677f2509476637e42f2528e2b15751f98109
dwieb <dwayne.harmon@gmail.com>
2018-03-22 23:03:57 -0500

    Need to seed a little data

---
+++ db/migrate/20180323034625_add_column_to_users.rb
@@ -0,0 +1,5 @@
+class AddColumnToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :display_name, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+User.create!(
+  display_name: 'D W A Y N E!',
+  email: 'dwayne@gmail.com',
+  encrypted_password: '1234'
+)
+
+Concept.create!(
+  title: 'Tic Tac Toe', 
+  description: 'Come on! Everyone knows Tic Tac Toe!',
+  resource_links: 'www.dwyn.nyc', 
+  user_id: 1
+)
+binding.pry
 Language.create!(
   name: 'Ruby',
   section_id: 1
@@ -13,24 +27,19 @@ Language.create!(
 
 Section.create!(
   title: 'CLI Gem',
-  description: 'Utilizing Object Oriented Programming to build and Publish a Ruby Gem.',
-  language_id: 1
+  description: 'Utilizing Object Oriented Programming to build and Publish a Ruby Gem.'
 )
 
 Concept.create!(
   title: 'Tic Tac Toe', 
   description: 'Come on! Everyone knows Tic Tac Toe!',
   resource_links: 'www.dwyn.nyc', 
-  user_id: 1, 
-  section_id: 1, 
-  language_id: 1
+  user_id: 1,
 )
 
 Concept.create!(
   title: 'Fibonacci Sequence',
   description: 'Enter a number and have the program generate the Fibonacci sequence to that number or to the Nth number',
   resource_links: 'www.dwyn.nyc',
-  user_id: 1,
-  section_id: 1,
-  language_id: 1
+  user_id: 1
 )
