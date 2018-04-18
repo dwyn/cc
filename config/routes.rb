6151ae9df6ac4256f090de252d81c6d484d3f3eb
dwieb <dwayne.harmon@gmail.com>
2018-04-17 19:47:11 -0500

    OmniAuth

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :concepts
   resources :sections, only: [:index, :show]
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
