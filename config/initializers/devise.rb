2b477b699610e03dcd5df7536d28bc7e6c93fc67
dwieb <dwayne.harmon@gmail.com>
2018-04-19 21:00:03 -0500

    OmniAuth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -276,5 +276,5 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :github, "GITHUB_KEY", "GITHUB_SECRET", callback_url: "http://localhost:3000/users/auth/github/callback"
+  config.omniauth :github, "GITHUB_KEY", "GITHUB_SECRET"#, callback_url: "http://localhost:3000/users/auth/github/callback"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180416043437) do
+ActiveRecord::Schema.define(version: 20180420015222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20180416043437) do
     t.boolean  "admin"
     t.string   "name"
     t.string   "uid"
+    t.string   "provider"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
