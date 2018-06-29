023e4b89fc2771bb3ce3921c2047cb836d4435ac
dwieb <dwayne.harmon@gmail.com>
2018-06-28 21:21:52 -0500

    refactored scope method

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,10 @@ class User < ApplicationRecord
     end
   end 
 
-  def favorite_concepts
-    self.concepts.where("favorited = true")
-  end
+  scope :favorited, -> { where(favorited: true) }
+  # def favorite_concepts
+  #   self.concepts.where("favorited = true")
+  # end
 
 
   def favorite_section
