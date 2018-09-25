b2200fc2060eaa3c8f6c87a907b558b91e1b0d2a
dwieb <dwayne.harmon@gmail.com>
2018-09-24 22:41:57 -0500

    Fixed show partial

--- app/assets/javascripts/formsubmit.js
+++ app/assets/javascripts/formsubmit.js
@@ -3,24 +3,27 @@
 // });
 
 $(function(){
-  $("#new_concept").on("submit", function (e) { // 1. Handle submit event
+  $(".new_concept").on("submit", function(e) { // 1. Handle submit event
     e.preventDefault();
     url = this.action
+
     params = { // 2. Structure parameters to be sent in the request 
       concept: {
-        title: document.querySelector("#concept_title").value }
-        // authenticity_token: document.querySelector("input[name=authenticity_token]")
+        title: document.querySelector("#concept_title").value
+      }
+      // authenticity_token: document.querySelector("input[name=authenticity_token]")
     }
+
     debugger
     fetch(url, { // 3. Submit post request
-      method: "POST", 
+      method: "POST",
       credentials: "same-origin",
-      headers: { "Content-Type": "application/json" }, 
-      body: JSON.stringify(params) 
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(params)
     })
     // 4. How to handle response (check fetch documentation first) 
     .then(response => response.json()); // parses response to JSON
   })
-})
+});
 
 
