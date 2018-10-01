06a1d9b11c1d3b29116fc7d28ae98a89db257e3b
dwieb <dwayne.harmon@gmail.com>
2018-09-30 22:50:27 -0500

    Fixed rendering of  in concepts#index view

--- app/assets/javascripts/userconcepts.js
+++ app/assets/javascripts/userconcepts.js
@@ -1,43 +1,49 @@
 class Concept {
-  constructor(id, title, description, user_id, section_id, favorited) {
+  constructor(id, title, description, user_id, favorited) {
     this.id = id;
     this.title = title;
     this.description = description;
     this.user_id = user_id;
-    this.section_id = section_id;
     this.favorited = favorited;
   }
 }
 
 $(function() { //Once document is ready ie page is loaded...
-  $.getJSON(this.href, function(data) { // HTTP GET request for some JSON. Upon success do...
-    $('.result').html(data);
-    {
-      let $userConcepts = $('.userConcepts');
-      data.forEach(concept => {
-        current_concept = new Concept(
-          concept.id,
-          concept.title,
-          concept.description,
-          concept.user_id,
-          concept.section_id,
-          concept.favorited
-        );
+  $('a.remainingConcepts').on('click', function (e) {   //Hijack click event
+    e.preventDefault();    //prevent browser from executing default action
+    $.getJSON(this.href, function (json) {   // HTTP GET request for some JSON. Upon success do...
+      let $remainingConcepts = $('#remainingConcepts');
 
-        let concept_link = $('<a></a> <br>');
-        let url = `users/${current_concept.user_id}/concepts/${current_concept.id}`;
-        concept_link.attr('href', url);
-        concept_link.html(current_concept.title);
-        $userConcepts.append(concept_link);
+      json.forEach(concept => {
+        current_concept = new Concept(
+        concept.id,
+        concept.title,
+        concept.description,
+        concept.user_id,
+        concept.favorited
+      );
+
+      let concept_link = $('<a></a> <br>'); // Empty anchor tag
+      let horizontal_line = "<hr> <br>"  // Horizontal line to append to div
+      concept_link.html(current_concept.title); // Set anchor tags title
+
+      let url = `users/${current_concept.user_id}/concepts/${current_concept.id}`;
+      debugger
+      concept_link.attr('href', url);
+
+      $remainingConcepts.append(concept_link);
+      $remainingConcepts.append(`${current_concept.description} <br>`);
+      $remainingConcepts.append(`${current_concept.favorited} <br>`);
+      $remainingConcepts.append(horizontal_line);
       });
-    }
+    })
   });
 });
 
 
-// // $(function () {   //Once document is ready ie page is loaded...
-// //   $('a.nextConcept').on('click', function (e) {   //Hijack click event
-// //     e.preventDefault();    //prevent browser from executing default action
+
+
+
 // //     $.get(this.href, function (json) {   // HTTP GET request for some JSON. Upon success do...
 // //       var $cardTitle = $('h2.card-title');   //Capture concept title
 // //       var $cardDescription = $('p.card-text');   //Capture concept description
