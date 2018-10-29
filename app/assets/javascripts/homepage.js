1b48096d2f9c591cb095494f04c21913636dfbd0
dwieb <dwayne.harmon@gmail.com>
2018-10-29 01:51:34 -0500

    Updated homepage json consumption

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,14 +1,15 @@
 $(function() {   //Once document is ready ie page is loaded...
   $('a.nextConcept').on('click', function(e) {   //Hijack click event
     e.preventDefault();    //prevent browser from executing default action
-    $.getJSON(this.href, function(json) {   // HTTP GET request for some JSON. Upon success do...
-      var $conceptTitle = $('h2.concept-title');   //Capture concept title
-      var $conceptUser = $('h2.concept-user');   //Capture concepts user
-      var $cardDesc = $('p.concept-desc');   //Capture concept description
-
-      $conceptTitle.html(json.title);   //Replace concept title
-      $conceptUser.html(json.user);   //Replace concept title
-      $cardDesc.html(json.description);   //Replace concept description
+    $.getJSON(this.href, function(json) {        //HTTP GET request for some JSON. Upon success do...
+      var $conceptTitle = $('h4.concept-title'); //Capture concept title
+      var $conceptUser = $('h5.concept-user');   //Capture concepts user
+      var $cardDesc = $('p.concept-desc');       //Capture concept description
+      // console.log(json.data)
+      // debugger
+      $conceptTitle.html(json.data.attributes.title)  //Replace concept title
+      $conceptUser.html(json.data.attributes.user);     //Replace concept user
+      $cardDesc.html(json.data.attributes.description); //Replace concept description
     });
   });
 });
