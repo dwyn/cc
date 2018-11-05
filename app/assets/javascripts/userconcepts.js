65d85e26d2262bf54cb85c9e915350704e311e0d
dwieb <dwayne.harmon@gmail.com>
2018-11-05 01:25:36 -0600

    fixed "unfavorited" button

--- app/assets/javascripts/userconcepts.js
+++ app/assets/javascripts/userconcepts.js
@@ -12,21 +12,25 @@ $(function() { //Once document is ready ie page is loaded...
   $('a.remainingConcepts').on('click', function (e) {   //Hijack click event
     e.preventDefault();    //prevent browser from executing default action
     $.getJSON(this.href, function (json) {   // HTTP GET request for some JSON. Upon success do...
-      let $remainingConcepts = $('#remainingConcepts');
+      let $notFavs = $('#notFavs');
 
-      json.forEach(concept => {
-          let currentConcept = new Concept(
-          concept.id,
-          concept.title,
-          concept.description,
-          concept.user_id,
-          concept.favorited
-        );
-        
-        let url = `/users/${currentConcept.user_id}/concepts/${currentConcept.id}`;
-        $remainingConcepts.append(`<a href="${url}"> ${currentConcept.title} </a> <br>`)
-        .toggle(200);
-      });
+      if ($notFavs.val() === "") {
+        json.forEach(concept => {
+            let currentConcept = new Concept(
+            concept.id,
+            concept.title,
+            concept.description,
+            concept.user_id,
+            concept.favorited
+          );
+          
+          let url = `/users/${currentConcept.user_id}/concepts/${currentConcept.id}`;
+          // debugger
+          $notFavs.append(`<a href="${url}"> ${currentConcept.title} </a> <br>`)
+        });
+      } else {
+        console.log("Hello!")
+      }
     })
   });
 });
